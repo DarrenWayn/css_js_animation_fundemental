b9b2fd0afcac37e41ce6761db7d7b316a3489e46
Darren Wayn <dwayn.dev@gmail.com>
2022-12-04 11:59:26 +0700

    learn windows properties

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 "use strict";
 
-const test = document.querySelectorAll("test");
+let h1 = document.querySelector("h1");
+h1.addEventListener("click", (ev) => {
+  /* scrollBy(x, y); */
+  /* scrollTo(x, y); */
+  /* scrollX(x); */
+  /* scrollY(y); */
+  console.group();
+  console.log("client", ev.clientX, ev.clientY);
+  // top of the visible screen
+  console.log("page", ev.pageX, ev.pageY);
+  // top of the document(webpage)
+  console.log("offset", h1.offsetLeft, h1.offsetTop);
+  console.log("screen window", window.screenX, window.screenY);
 
-console.log(test);
+  console.log("screen ev", ev.screenX, ev.screenY);
+  console.groupEnd();
+  window.scrollBy(0, 600);
+});
+
+window.addEventListener("scroll", (ev) => {
+  console.group();
+  console.log("client", ev.clientX, ev.clientY);
+  // top of the visible screen
+  console.log("page", ev.pageX, ev.pageY);
+  // top of the document(webpage)
+  console.log("offset", h1.offsetLeft, h1.offsetTop);
+  console.log("screen window", window.screenX, window.screenY);
+
+  console.log("screen ev", ev.screenX, ev.screenY);
+  console.groupEnd();
+
+  setTimeout(() => {
+    window.scrollTo(0, 0);
+  }, 2000);
+});
